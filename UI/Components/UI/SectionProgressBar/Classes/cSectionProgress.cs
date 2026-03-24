d5168cdeda524f6d1db42d1f33d96da19c9f5afa
Taylor Kelley <taylorgkelley04@gmail.com>
2026-03-24 08:34:33 -0500

    fix: fixing compile and build time errors by removing unnecessary @rendermode directive for components

--- UI/Components/UI/SectionProgressBar/Classes/cSectionProgress.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorUI.Components.UI.SectionProgressBar.Classes
-{
-    public class cSectionProgress
-    {
-        public System.String mstrTitle { get; set; }
-        public System.Int32 mintProgressPercentage { get; set; } = 0;
-    }
-}
